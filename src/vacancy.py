bf3d0bc3dc67a67f9c703daf56dc6920d2cf6e85
dmitrijsemenkin <dmirijsemenkin@gmail.com>
2024-05-20 23:06:26 +0300

    remove magic methods

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,5 +1,17 @@
 class Vacancy:
+    """
+    Represents a vacancy in the HH.ru system.
 
+    Attributes:
+        name (str): The title of the vacancy.
+        salary (dict): The salary of the vacancy in the format {from: int, to: int}.
+        currency (str): The currency of the salary.
+        url (str): The URL of the vacancy.
+        requirement (str, optional): The requirement for the vacancy. Defaults to 'Информация отсутствует'.
+
+    Methods:
+        __repr__ (str): Returns a string representation of the object.
+    """
     def __init__(self, name, salary, currency, url, requirement='Информация отсутствует'):
         self.name = name
         self.salary = salary
@@ -9,39 +21,3 @@ class Vacancy:
 
     def __repr__(self):
         return f'Name: {self.name}\nSalary: {self.salary}\nCurrency: {self.currency}\nURL: {self.url}\nRequirement: {self.requirement}'
-    
-    def __gt__(self, other):
-        """
-        Compare two Vacancy objects based on their salary.
-
-        Parameters:
-            other (Vacancy): The Vacancy object to compare with.
-
-        Returns:
-            Vacancy or str: If both salaries are not None and self.salary['to'] > other.salary['to'], return self.
-            Otherwise, return 'Зарплата не указана' if either salary is None.
-        """
-        if self.salary is not None and other.salary is not None:
-            if self.salary['to'] > other.salary['to']:
-                return self
-            else:
-                return other
-        return 'Зарплата не указана'
-
-    def __lt__(self, other):
-        """
-        Compare two Vacancy objects based on their salary.
-
-        Parameters:
-            other (Vacancy): The Vacancy object to compare with.
-
-        Returns:
-            Vacancy or str: If both salaries are not None and self.salary['to'] < other.salary['to'], return self.
-            Otherwise, return 'Зарплата не указана' if either salary is None.
-        """
-        if self.salary is not None and other.salary is not None:
-            if self.salary['to'] < other.salary['to']:
-                return self
-            else:
-                return other
-        return 'Зарплата не указана'
